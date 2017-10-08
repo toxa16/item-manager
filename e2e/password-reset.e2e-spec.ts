570200178989feb4f606551819caeff4723a96ef
toxa16 <toxa16@gmail.com>
2017-10-08 16:34:59 +0300

    auth routing e2e

--- e2e/password-reset.e2e-spec.ts
+++ e2e/password-reset.e2e-spec.ts
@@ -1,10 +1,20 @@
 import {browser, by, element} from 'protractor';
 
-describe('Sign Up Page', () => {
+describe('Password Reset Page', () => {
+
+  beforeEach(() => {
+    browser.get('/password-reset');
+  });
 
   it('should contain Password Reset Page contents', () => {
     browser.get('/password-reset');
     const contents = element(by.id('passwordResetPage'));
     expect(contents.isPresent()).toBe(true);
+  });
+
+  it('should redirect to Sign In Page on #cancelLink click', () => {
+    const signUpLink = element(by.id('cancelLink'));
+    signUpLink.click();
+    expect(browser.getCurrentUrl()).toContain('/sign-in');
   });
 });
--- e2e/sign-up.e2e-spec.ts
+++ e2e/sign-up.e2e-spec.ts
@@ -2,9 +2,18 @@ import {browser, by, element} from 'protractor';
 
 describe('Sign Up Page', () => {
 
-  it('should contain Sign Up Page contents', () => {
+  beforeEach(() => {
     browser.get('/sign-up');
+  });
+
+  it('should contain Sign Up Page contents', () => {
     const contents = element(by.id('signUpPage'));
     expect(contents.isPresent()).toBe(true);
+  });
+
+  it('should redirect to Sign In Page on #signInLink click', () => {
+    const signUpLink = element(by.id('signInLink'));
+    signUpLink.click();
+    expect(browser.getCurrentUrl()).toContain('/sign-in');
   });
 });
