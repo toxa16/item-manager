2dc7aac4fba5c6f346847948ec8a98f771782999
toxa16 <toxa16@gmail.com>
2017-10-10 11:09:48 +0300

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
-import {SignInComponent} from './auth/sign-in/sign-in.component';
 import {HomepageComponent} from './homepage/homepage.component';
-import {SignUpComponent} from './auth/sign-up/sign-up.component';
-import {PasswordResetComponent} from './auth/password-reset/password-reset.component';
-import {InputComponent} from './auth/input.component';
 import {AppRoutingModule} from './app-routing.module';
-
-const routes: Routes = [
-  { path: 'password-reset', component: PasswordResetComponent },
-  { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  { path: '', component: HomepageComponent },
-];
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputComponent,
     HomepageComponent,
-    PasswordResetComponent,
-    SignInComponent,
-    SignUpComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    AuthModule,
     AppRoutingModule,
   ],
   providers: [],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,4 +1,25 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
-@NgModule()
+import {PasswordResetComponent} from './password-reset/password-reset.component';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {InputComponent} from './input.component';
+
+@NgModule({
+  declarations: [
+    InputComponent,
+    PasswordResetComponent,
+    SignInComponent,
+    SignUpComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+})
 export class AuthModule {}
