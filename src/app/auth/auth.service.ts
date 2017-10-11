ff3b8a0a8b970618d07450f3d19aa2a5dd53df5c
toxa16 <toxa16@gmail.com>
2017-10-11 12:16:38 +0300

    mock users

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,37 +1,39 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
 
 import {NoConnection} from '../common/errors';
-
+import {User} from './user';
+import {mockUsers} from './mock-users';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private http: Http) {}
+  private users: User[] = mockUsers;
+  private latency = 1500;
 
   checkEmail(email: string): Observable<boolean> {
 
-    return this.http.get('http://localhost:3000')
+    /*return this.http.get('http://localhost:3000')
       .map(res => {
         console.log(res.status);
         return res.ok;
       })
       .catch(err => {
         throw new Error();
-      });
+      });*/
 
-    //return Observable.of({ emailTaken: true }).delay(1500);
-    //return Observable.of(true).delay(1500);
+    const foundUser = this.users.find(user => user.email === email);
+    return Observable.of(!foundUser).delay(this.latency);
   }
 
   signUp(email: string, password: string): Observable<null> {
-    return Observable.of(null).delay(1500);
+    const newUser: User = { email, password };
+    this.users.push(newUser);
+    return Observable.of(null).delay(this.latency);
   }
 }
---
+++ src/app/auth/mock-users.ts
@@ -0,0 +1,7 @@
+import {User} from './user';
+
+export const mockUsers: User[] = [
+  { email: 'uno@email.com', password: '1111' },
+  { email: 'dos@email.com', password: '2222' },
+  { email: 'tres@email.com', password: '3333' },
+];
